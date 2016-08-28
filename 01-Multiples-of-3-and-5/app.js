c45081bd3411e42f14e27aaa0eaeb192622f1e84
necmttn <necmettin.karakaya@gmail.com>
2016-08-28 18:45:15 +0800

    added some comments

--- 01-Multiples-of-3-and-5/app.js
+++ 01-Multiples-of-3-and-5/app.js
@@ -1,14 +1,13 @@
-// Task
-// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-// Find the sum of all the multiples of 3 or 5 below 1000.
 
 
+// Creating Numbers Array.
 var numbers = []
 
 for (var i = 0; i < 1000; i++){
   numbers.push(i)
 }
 
+// Function which returns Summary of 3 and 5 times.
 var sumOfThreeAndFiveTimes = numbers.filter(function(number){
   return number % 3 === 0 || number % 5 === 0;
 }).reduce(function(a,b){
@@ -16,3 +15,5 @@ var sumOfThreeAndFiveTimes = numbers.filter(function(number){
 },0);
 
 console.log(sumOfThreeAndFiveTimes);
+
+// TODO:Refactor this code using only Reduce function
